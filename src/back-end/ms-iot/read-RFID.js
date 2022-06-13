63a7136b9d1d5667bf5a784f9e255f9ae6bea387
Djihane <djihaneien@gmail.com>
2022-06-13 14:46:36 +0100

    last

--- src/back-end/ms-iot/read-RFID.js
+++ src/back-end/ms-iot/read-RFID.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/back-end/ms-iot/Read-RFID.js
 const express = require("express");
 const socketIo = require("socket.io")
 const http = require("http")
@@ -23,12 +22,6 @@ io.on("connection",(socket)=>{
 
 
 
-=======
-var http = require('http');
-var fs = require('fs');
-var amqp = require('amqplib/callback_api')
-var index = fs.readFileSync( 'index.html');
->>>>>>> 1cf51db8f0a1aa7597891593158a2b84943476dc:src/back-end/ms-iot/read-RFID.js
 
 var SerialPort = require('serialport');
 const parsers = SerialPort.parsers;
@@ -62,7 +55,7 @@ var io = require('socket.io').listen(app);**/
 
 parser.on('data', function(data) {
     console.log('Received data from port: ' + data);
-<<<<<<< HEAD:src/back-end/ms-iot/Read-RFID.js
+
 
     io.to("clock-room").emit('data', data);
     
@@ -72,9 +65,3 @@ server.listen(5000, err=> {
     if(err) console.log(err)
     console.log("Server running on Port ", 5000)
   })
-=======
-    io.emit('data', data);
-    
-});
-app.listen(3001);
->>>>>>> 1cf51db8f0a1aa7597891593158a2b84943476dc:src/back-end/ms-iot/read-RFID.js
